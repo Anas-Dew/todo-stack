2d5383711daa93f033d16cced91a168ee28f7909
anas-dew <mailtoanas@duck.com>
2022-12-26 16:14:12 +0530

    documentation

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-      }
+        documentation: resolve(__dirname, 'documentation.html')}
     }
   }
 })
